c327d8857905aa32ed4c4f6ec744b0000ff895ce
Karol W <xardonik@gmail.com>
2020-08-04 22:19:07 +0200

    Modified layout of List Connect onRemove function

--- src/components/todo_list/TodoList.js
+++ src/components/todo_list/TodoList.js
@@ -1,21 +1,43 @@
 import React from 'react';
 //import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
+import {ROUTER_TODO_LIST} from "../../constants";
 
 const propTypes = {
 	id: PropTypes.string,
 	name: PropTypes.string,
+	onRemove: PropTypes.func,
 };
 
 const defaultProps = {};
 
 const TodoList = (props) => {
-	const {id, name} = props;
+	const {id, name, onRemove} = props;
 
 	return (
 		<>
-			{id}
-			{name}
+			<div className="row my-3">
+				<div className="col">{name}</div>
+				<div className="col-auto">{id}</div>
+				<div className="col-auto">
+					<Link
+						to={`${ROUTER_TODO_LIST}/${id}`}
+						className="btn btn-primary"
+
+					>
+						Select list
+					</Link>
+				</div>
+				<div className="col-auto">
+					<div
+						className="btn btn-danger"
+						onClick={onRemove}
+					>
+						Remove list
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
