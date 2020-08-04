974d92b12dafed329959c27c7342e884ed67b707
Karol W <xardonik@gmail.com>
2020-08-04 22:13:09 +0200

    Added hook to set current list id Refacotr names of components

--- src/components/todo_item/TodoItemsWrapper.js
+++ src/components/todo_item/TodoItemsWrapper.js
@@ -1,20 +1,29 @@
-import React from 'react';
-//import {connect} from 'react-redux';
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import TodoAddField from "./TodoItemAdd";
-import TodoList from "./TodoItems";
+import TodoItemAdd from "./TodoItemAdd";
+import TodoItems from "./TodoItems";
+import {useParams} from "react-router-dom";
+import {todoListSelect} from "../../redux/actions/todo_list";
 
 const propTypes = {};
 
 const defaultProps = {};
 
 const TodoItemsWrapper = (props) => {
-	const {} = props;
+	const {todoListSelect} = props;
+	const {todo_list_id} = useParams();
+
+	useEffect(() => {
+		if (todo_list_id) {
+			todoListSelect(todo_list_id);
+		}
+	}, [todoListSelect, todo_list_id]);
 
 	return (
 		<>
-			<TodoAddField/>
-			<TodoList/>
+			<TodoItemAdd/>
+			<TodoItems/>
 		</>
 	);
 };
@@ -22,9 +31,11 @@ const TodoItemsWrapper = (props) => {
 TodoItemsWrapper.propTypes = propTypes;
 TodoItemsWrapper.defaultProps = defaultProps;
 
-//const mapStateToProps = state => {};
+const mapStateToProps = state => ({});
 
-//const actionCreators = {};
+const actionCreators = {
+	todoListSelect,
+};
 
-//export default connect(mapStateToProps, actionCreators)(TodoItemsWrapper);
-export default TodoItemsWrapper;
+export default connect(mapStateToProps, actionCreators)(TodoItemsWrapper);
+// export default TodoItemsWrapper;
