f58b38273078541eb1df36d97deb574253b28a62
Karol W <xardonik@gmail.com>
2020-08-04 22:18:31 +0200

    Added filtering for todo_list Added action to remove list Modified layout

--- src/components/todo_list/TodoLists.js
+++ src/components/todo_list/TodoLists.js
@@ -2,23 +2,32 @@ import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import TodoList from "./TodoList";
+import {filter} from "ramda";
+import {todoListRemove} from "../../redux/actions/todo_list";
 
 const propTypes = {};
 
 const defaultProps = {};
 
 const TodoLists = (props) => {
-	const {todo_lists} = props;
+	const {todo_lists, todoListRemove} = props;
+
+	const onTodoListRemove = (id) => {
+		todoListRemove(id);
+	}
 
 	return (
 		<>
-			{todo_lists.map(todo_list => (
-				<TodoList
-					key={todo_list.id}
-					id={todo_list.id}
-					name={todo_list.name}
-				/>
-			))}
+			<div className="container">
+				{todo_lists.map(todo_list => (
+					<TodoList
+						key={todo_list.id}
+						id={todo_list.id}
+						name={todo_list.name}
+						onRemove={() => onTodoListRemove(todo_list.id)}
+					/>
+				))}
+			</div>
 		</>
 	);
 };
@@ -27,14 +36,18 @@ TodoLists.propTypes = propTypes;
 TodoLists.defaultProps = defaultProps;
 
 const mapStateToProps = state => {
-	const {todo_lists} = state.todo;
+	const {todo_lists, todo_group_selected} = state.todo;
+
+	const todo_lists_filtered = filter(todo_list => todo_list.todoGroupID === todo_group_selected, todo_lists);
 
 	return {
-		todo_lists,
+		todo_lists: todo_lists_filtered,
 	}
 };
 
-const actionCreators = {};
+const actionCreators = {
+	todoListRemove,
+};
 
 export default connect(mapStateToProps, actionCreators)(TodoLists);
 // export default TodoLists;
